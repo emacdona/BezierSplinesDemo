076159a6f90fb77d4bab1f3a41a793bcb56fcd8f
Ed MacDonald <edmacdonald555@gmail.com>
2010-04-22 15:10:42 -0400

    Simplified project6.m file to call plotGMU function

--- project6.m
+++ project6.m
@@ -4,17 +4,6 @@ clf;
 
 hold on;
 
-[gx,gy] = g;
-[mx,my] = m;
-[ux,uy] = u;
-
-spacing = 50;
-
-c = 1;
-showPoints = 0;
-
-maxGx = bezierPlot(gca, gx, gy, c, showPoints);
-maxMx = bezierPlot(gca, mx + maxGx + spacing, my, c, showPoints);
-maxUx = bezierPlot(gca, ux + maxMx + spacing, uy, c, showPoints);
+plotGMU(gca, 50, 0, 0);
 
 hold off;
